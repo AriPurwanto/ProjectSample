b5d9c77f9beb8b2e1b0231aa91b5d8782386c6a5
Ari Purwanto <higuain.arie@gmail.com>
2021-03-04 11:16:35 +0700

    first commit

---
+++ WebApplication1/Soal_Binus.aspx
@@ -0,0 +1,123 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Soal_Binus.aspx.cs" Inherits="WebApplication1.Soal_Binus" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Invoice Form</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>
+            <table>
+                <tr>
+                   <td style="left: 10px; width: 120px">No Invoice :</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:TextBox ID="textNoInvoice" runat="server" Width="200px" CssClass="form-control"></asp:TextBox>
+                   </td>
+                   <td style="left: 120px; width: 30px">
+                       <asp:Button ID="btnView" runat="server" Text="View" Width="100px" CssClass="form-control" OnClick="btnView_Click" />
+                   </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td>Invoice</td>
+                </tr>
+                <tr>
+                   <td style="left: 10px; width: 120px">No Date :</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:TextBox ID="textInvoiceDate" runat="server" Width="200px" CssClass="form-control"></asp:TextBox>
+                   </td>
+                   <td style="left: 120px; width: 100px">Ship To :</td>
+                   <td style="left: 170px; width: 30px">
+                       <asp:TextBox ID="textShipTo" runat="server" Height="200px" Width="200px" CssClass="form-control"></asp:TextBox>
+                       <%--<asp:Label ID="labelShipTo" runat="server" Width="200px" CssClass="form-control"></asp:Label>--%>
+                   </td>
+                </tr>
+                <tr>
+                   <td style="left: 10px; width: 120px">To :</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:TextBox ID="textTo" runat="server" Height="200px" Width="200px" CssClass="form-control"></asp:TextBox>
+                   </td>
+                   <td style="left: 120px; width: 100px">Payment Type :</td>
+                   <td style="left: 170px; width: 30px">
+                       <asp:DropDownList ID="ddPaymentType" runat="server" Width="200px" CssClass="form-control"></asp:DropDownList>
+                   </td>
+                </tr>
+                <tr>
+                   <td style="left: 10px; width: 120px">Sales Name :</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:DropDownList ID="ddSalesName" runat="server" Width="200px" CssClass="form-control"></asp:DropDownList>
+                   </td>
+                </tr>
+                <tr>
+                   <td style="left: 10px; width: 120px">Courier :</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:DropDownList ID="ddCourier" runat="server" Width="200px" CssClass="form-control"></asp:DropDownList>
+                   </td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <asp:GridView ID="GridResult" runat="server" AutoGenerateColumns="false">
+                <Columns>
+                    <asp:BoundField DataField="Item" HeaderText ="Item"/>
+                    <asp:BoundField DataField="WeightKg" HeaderText ="Weight(kg)"/>
+                    <asp:BoundField DataField="Qty" HeaderText ="Qty)"/>
+                    <asp:BoundField DataField="Price" HeaderText ="Unit Price"/>
+                    <asp:BoundField DataField="Total" HeaderText ="Total"/>
+                </Columns>
+            </asp:GridView>
+        </div>
+        <div>
+            <table>
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+            </table>
+        </div>
+        <div>
+            <table>
+                <tr>
+                   <td style="left: 10px; width: 120px">Sub Total</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:Label ID="lblSubTotal" Width="200px" runat="server" CssClass="form-control"></asp:Label>
+                   </td>
+                </tr>
+                <tr>
+                   <td style="left: 10px; width: 120px">Courier Fee</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:Label ID="lblCourierFee" Width="200px" runat="server" CssClass="form-control"></asp:Label>
+                   </td>
+                </tr>
+                <tr>
+                   <td style="left: 10px; width: 120px">Total</td>
+                   <td style="left: 60px; width: 30px">
+                       <asp:Label ID="lblTotal" Width="200px" runat="server" CssClass="form-control"></asp:Label>
+                   </td>
+                </tr>
+                <tr>
+                    <td>&nbsp;</td>
+                </tr>
+                <tr>
+                    <td style="left: 10px; width: 120px"></td>
+                    <td>
+                       <asp:Button ID="btnSave" runat="server" Text="Save" Width="100px" CssClass="btn btn-md btn-default" OnClick="btnSave_Click" />
+                    </td>
+               </tr>
+            </table>
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication1/Soal_Binus.aspx.cs
@@ -0,0 +1,259 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace WebApplication1
+{
+    public partial class Soal_Binus : System.Web.UI.Page
+    {
+        private string strConString = ConfigurationManager.ConnectionStrings["conString"].ConnectionString;
+        private string message;
+
+        public void dataLoad_Grid()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ToString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "spLoadGridView";
+            cmd.Parameters.Add("@InvoiceNo", SqlDbType.VarChar).Value = textNoInvoice.Text.Trim();
+            cmd.Connection = con;
+
+            try
+            {
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    if (dt.Rows.Count > 0)
+                    {
+                        GridResult.DataSource = dt;
+                        GridResult.DataBind();
+                    }
+
+                    con.Close();
+                
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Message", "alert('Oops!! following error occured : " + message.ToString() + "');", true);
+            }
+        }
+
+        public void dataLoad_Sales()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(strConString))
+                {
+                    SqlCommand cmd = new SqlCommand("spLoadSales", con);
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    con.Close();
+
+                    ddSalesName.DataSource = dt;
+                    ddSalesName.DataValueField = "SalesName";
+                    ddSalesName.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+        }
+
+        public void dataLoad_Courier()
+        {
+            try
+            {
+                using (SqlConnection con = new SqlConnection(strConString))
+                {
+                    SqlCommand cmd = new SqlCommand("spLoadCourier", con);
+                    con.Open();
+                    SqlDataAdapter da = new SqlDataAdapter(cmd);
+                    DataTable dt = new DataTable();
+                    da.Fill(dt);
+                    con.Close();
+
+                    ddCourier.DataSource = dt;
+                    ddCourier.DataValueField = "CourierName";
+                    ddCourier.DataBind();
+                }
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+        }
+
+        public void dataLoad_PaymentType()
+        {
+            ddPaymentType.Items.Add(new ListItem("Cash", "Cash"));
+            ddPaymentType.Items.Add(new ListItem("COD", "COD"));
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+          if (!IsPostBack)
+           {
+              //dataLoad_Grid();
+              dataLoad_Sales();
+              dataLoad_Courier();
+              dataLoad_PaymentType();
+           }
+        }
+
+        public void SubTotal()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ToString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "spSubTotal";
+            cmd.Parameters.Add("@NoInvoice", SqlDbType.VarChar).Value = textNoInvoice.Text.Trim();
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    lblSubTotal.Text = dt.Rows[0].ItemArray[1].ToString();
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Message", "alert('Oops!! following error occured : " + message.ToString() + "');", true);
+            }
+        }
+
+        public void CourierFee()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ToString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "spCourierFee";
+            cmd.Parameters.Add("@NoInvoice", SqlDbType.VarChar).Value = textNoInvoice.Text.Trim();
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    lblCourierFee.Text = dt.Rows[0].ItemArray[1].ToString();
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Message", "alert('Oops!! following error occured : " + message.ToString() + "');", true);
+            }
+        }
+
+        public void Total()
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ToString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "spSubTotal";
+            cmd.Parameters.Add("@NoInvoice", SqlDbType.VarChar).Value = textNoInvoice.Text.Trim();
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+                if (dt.Rows.Count > 0)
+                {
+                    lblTotal.Text = dt.Rows[0].ItemArray[1].ToString();
+                }
+
+                con.Close();
+
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+                ScriptManager.RegisterStartupScript(this, this.GetType(), "Message", "alert('Oops!! following error occured : " + message.ToString() + "');", true);
+            }
+        }
+
+        protected void btnView_Click(object sender, EventArgs e)
+        {
+            SqlConnection con = new SqlConnection(ConfigurationManager.ConnectionStrings["conString"].ToString());
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.CommandText = "spFindInvoice";
+            cmd.Parameters.Add("@InvoiceNo", SqlDbType.VarChar).Value = textNoInvoice.Text.Trim();
+            cmd.Connection = con;
+
+            try
+            {
+                con.Open();
+                cmd.ExecuteNonQuery();
+
+                DataSet ds = new DataSet();
+                SqlDataAdapter da = new SqlDataAdapter(cmd);
+                da.Fill(ds);
+
+                DataTable dt = ds.Tables[0];
+                if (dt.Rows.Count > 0)
+                {
+                    textInvoiceDate.Text = dt.Rows[0].ItemArray[1].ToString();
+                    textTo.Text = dt.Rows[0].ItemArray[2].ToString();
+                    textShipTo.Text = dt.Rows[0].ItemArray[3].ToString();
+                    ddPaymentType.Text = dt.Rows[0].ItemArray[4].ToString();
+                    ddSalesName.Text = dt.Rows[0].ItemArray[5].ToString();
+                    ddCourier.Text = dt.Rows[0].ItemArray[6].ToString();
+                }
+
+                dataLoad_Grid();
+                CourierFee();
+                SubTotal();
+                Total();
+            }
+            catch (Exception ex)
+            {
+                message = ex.Message;
+            }
+            finally
+            {
+                con.Close();
+                con.Dispose();
+            }
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
